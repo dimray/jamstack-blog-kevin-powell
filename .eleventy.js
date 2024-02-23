54a1b089e304df0136a19c0eb069187fc9bea9b8
happyw3bsite <158301159+happyw3bsite@users.noreply.github.com>
2024-02-23 11:18:38 +0000

    eleventified static pages

---
+++ .eleventy.js
@@ -0,0 +1,21 @@
+module.exports = function (eleventyConfig) {
+  const { DateTime } = require("luxon");
+  eleventyConfig.addPassthroughCopy("./src/assets");
+  eleventyConfig.addPassthroughCopy("./src/style.css");
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`);
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED);
+  });
+
+  return {
+    markdownTemplateEngine: "njk",
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    dir: {
+      input: "src",
+      output: "public",
+      includes: "_includes",
+      layouts: "_layouts",
+    },
+  };
+};
